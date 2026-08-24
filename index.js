e9efcc5b2d794ca7f34414d67d19d2531859bfbd
Muhammad-Basit2 <muhammadbasit9274@gmail.com>
2026-08-25 01:21:03 +0500

    add logos and icons

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 800,
-    icon: path.join(__dirname, 'assets/pos logo.jpg'),
+    icon: path.join(__dirname, 'assets/poslogo.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
